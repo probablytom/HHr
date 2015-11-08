f1e6056088ca29efcc37691c3376c39c93ac5a87
Tom Wallis <tom@tom-wallis.com>
2015-11-08 17:28:46 +0000

    Project template get

---
+++ wear/src/main/java/io/hitchhikr/hitchhikr/LiftFound.java
@@ -0,0 +1,17 @@
+package io.hitchhikr.hitchhikr;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class LiftFound extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+        mTextView = (TextView) findViewById(R.id.text);
+    }
+}
